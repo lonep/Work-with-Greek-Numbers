3163163cb96698f12283b4a8ae57755dcf9fa2e0
lonep <djoefrit@gmail.com>
2020-03-01 14:17:48 +0300

    Program

---
+++ cmake-build-debug/CMakeFiles/Laba1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba1.dir/main.cpp.obj"
+  "Laba1.exe"
+  "Laba1.exe.manifest"
+  "Laba1.pdb"
+  "libLaba1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laba1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
